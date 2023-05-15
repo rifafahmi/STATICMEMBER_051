fe774efe8d9f6b8506435999f6c38b6d6776eb04
rifa <rifafahmi21.com@gmai.com>
2023-05-15 11:44:47 +0700

    alahsiah

--- STATICMEMBER_051/STATICMEMBER_051.cpp
+++ STATICMEMBER_051/STATICMEMBER_051.cpp
@@ -16,5 +16,25 @@ public:
 int mahasiswa::nim = 0;
  
 void mahasiswa::setID() {
+	id = ++nim;
 
+}
+
+void mahasiswa::printAll() {
+	cout << " ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("andi kurniawan");
+	mahasiswa mhs4("joko purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
