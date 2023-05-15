a864832118859d018a08c1d1c25c700543b4902e
rifa <rifafahmi21.com@gmai.com>
2023-05-15 11:29:15 +0700

    Add project files.

---
+++ STATICMEMBER_051/STATICMEMBER_051.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include	<string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) : nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+
+}
